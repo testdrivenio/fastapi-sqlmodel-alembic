bb6ae7a4f84c7957531533994cfcd2effda8b6c6
Michael Herman <hermanmu@gmail.com>
2021-09-08 18:33:52 -0500

    remove import

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from app.db import get_session, create_db_and_tables
+from app.db import get_session
 from app.models import Song, SongCreate
 
 app = FastAPI()
