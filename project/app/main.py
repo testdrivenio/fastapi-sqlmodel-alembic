c31596637bd2860622210a9733f578782e404c68
Michael Herman <hermanmu@gmail.com>
2021-09-08 18:28:28 -0500

    init

---
+++ project/app/main.py
@@ -0,0 +1,28 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.db import get_session, create_db_and_tables
+from app.models import Song, SongCreate
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong!"}
+
+
+@app.get("/songs", response_model=list[Song])
+async def get_songs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
+
+
+@app.post("/songs")
+async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
+    song = Song(name=song.name, artist=song.artist, year=song.year)
+    session.add(song)
+    await session.commit()
+    return song
